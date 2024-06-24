1766beb2756a594b207624a6172d440001d62eea
Y.Takeuchi <kaidouji85@gmail.com>
2024-06-24 23:31:24 +0900

    サインインができるようになった

---
+++ configtemplate.ts
@@ -0,0 +1,10 @@
+export const config = {
+  /** Cognito ユーザープールID */
+  userPoolId: 'ap-northeast-1_XXXXXXXXXX',
+  /** Cognito ユーザープール クライアントID */
+  userPoolClientId: 'XXXXXXXXXX',
+  /** Cognito Hosted UI ドメイン */
+  hostedUIDomain: 'XXXXXXXXXX.auth.ap-northeast-1.amazoncognito.com',
+  /** Hosted UI ログイン後のリダイレクトURL */
+  redirectURL: 'http://localhost:5173',
+};
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import {Amplify} from 'aws-amplify'
+import {getCurrentUser, signInWithRedirect} from "aws-amplify/auth"
+
 import './style.css'
 import {config} from '../config'
 
@@ -7,6 +9,15 @@ Amplify.configure({
     Cognito: {
       userPoolId: config.userPoolId,
       userPoolClientId: config.userPoolClientId,
+      loginWith: {
+        oauth: {
+          domain: config.hostedUIDomain,
+          scopes: ['openid'],
+          redirectSignIn: [config.redirectURL],
+          redirectSignOut: [config.redirectURL],
+          responseType: 'code',
+        }
+      }
     }
   }
 });
@@ -14,5 +25,15 @@ Amplify.configure({
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     hello
+    <button id="sign-in">サインイン（Hosted UI）</button>
   </div>
 `
+
+document.querySelector('#sign-in')!.addEventListener('click', async () => {
+  await signInWithRedirect();
+});
+
+window.addEventListener("load", async () => {
+  const user = await getCurrentUser();
+  console.log(user);
+});
